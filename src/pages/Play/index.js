c578cae653eb79d92a8c63e2e7684e2276767adb
Pourghannad <mhpourghannad@gmail.com>
2022-11-04 14:58:36 +0330

    add playContainerRef

--- src/pages/Play/index.js
+++ src/pages/Play/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import style from "./style.module.scss";
 import Grid from './component/Grid';
 import { data } from './level.js';
 const Play = () => {
+    const playContainerRef = useRef(null);
+    useEffect(() => {
+        if (playContainerRef.current) {
+            setTimeout(() => {
+                playContainerRef.current.scrollLeft = 80;
+                setTimeout(() => {
+                    playContainerRef.current.scrollLeft = 0;
+                }, 350);
+            }, 700);
+        }
+    }, []);
     return (
-        <div className={style["play-main-container"]}>
+        <div ref={playContainerRef} className={style["play-main-container"]}>
             <div className={style["page-container"]}>
                 <span className={style["text"]}>Standard</span>
                 <Grid data={data["1"].standard} />
