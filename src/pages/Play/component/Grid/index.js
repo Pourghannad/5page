7b075779acd165a8dadfd08ddd36913583f7843a
Pourghannad <mhpourghannad@gmail.com>
2022-11-06 01:22:07 +0330

    add standard condition for grid component

--- src/pages/Play/component/Grid/index.js
+++ src/pages/Play/component/Grid/index.js
@@ -8,17 +8,28 @@ const Grid = (props) => {
         if (!props.standard)  {
             if (!selected.includes(param.uuid)) {
                 setSelected([...selected, param.uuid])
+            } else {
+                setSelected(selected.filter((item) => {
+                    return item !== param.uuid
+                }));
             }
         }
     }
     return (
-        <div className={style["grid-container"]}>
+        <div className={classnames(style["grid-container"], {[style["standard"]]: props.standard})}>
             <div className={style["items-container"]}>
                 {props.data.grid.map((item, key) => {
                     const itemCss = cloneObject(item.css);
                     const defaultCss = cloneObject(props.data.defaultItem);
                     return (
-                        <div key={key} onClick={() => itemHandleClick(item)} className={classnames(style["item"], {[style["active"]]: item.active, [style["selected"]]: selected.includes(item.uuid)})}>
+                        <div
+                            key={key}
+                            onClick={() => itemHandleClick(item)}
+                            className={classnames(style["item"], {
+                                [style["active"]]: item.active,
+                                [style["selected"]]: selected.includes(item.uuid),
+                            })}
+                        >
                             <span style={Object.assign(defaultCss, itemCss)}></span>
                         </div>
                     );
