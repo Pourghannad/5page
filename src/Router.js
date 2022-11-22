0686af2467aaf4a5510ca1c2ffaa93b181f06eb5
Pourghannad <mhpourghannad@gmail.com>
2022-11-22 12:25:10 +0330

    remove /home route

--- src/Router.js
+++ src/Router.js
@@ -3,15 +3,11 @@ import { Switch, Route } from "react-router-dom";
 import { withRouter } from "react-router";
 import Home from "./pages/Home";
 import Play from "./pages/Play";
-import RootPage from "./pages/Root";
 
 const RouterSwitch = () => {
     return (
         <Switch>
           <Route exact path="/">
-            <RootPage />
-          </Route>
-          <Route exact path="/home">
             <Home />
           </Route>
           <Route exact path="/play">
