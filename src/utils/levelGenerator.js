7a0443794fbc90329534c3cf9ef6ab530c95d3eb
Pourghannad <mhpourghannad@gmail.com>
2022-11-23 13:31:48 +0330

    add level two

---
+++ src/utils/levelGenerator.js
@@ -0,0 +1,369 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const levelGenerator = () => {
+    const Level = {
+        "1": {
+            "standard": {
+                "standard": true,
+                "defaultItem": {
+                        "width": "30px",
+                        "height": "30px",
+                        "backgroundColor": "#00B2A9",
+                },
+                "grid": [{
+                    "css": {
+                        "width": "6px",
+                        "height": "6px",
+                        
+                    },
+                    "active": true
+                },{
+                    "css": {}
+                },{
+                    "css": {
+                        "width": "20px"
+                    }
+                },{
+                    "css": {}
+                },{
+                    "css": {}
+                },{
+                    "css": {}
+                },{
+                    "css": {}
+                },{
+                    "css": {
+                        "width": "40px"
+                    }
+                },{
+                    "css": {}
+                },{
+                    "css": {}
+                },{
+                    "css": {}
+                },{
+                    "css": {
+                        "width": "40px"
+                    }
+                }]
+            },
+            "page1": {
+                "defaultItem": {
+                    "width": "30px",
+                    "height": "30px",
+                    "backgroundColor": "#00B2A9"
+            },
+            "grid": [{
+                "uuid": uuidv4(),
+                "css": {},
+            },{
+                "uuid": uuidv4(),
+                "css": {
+                    "width": "50px"
+                }
+            },{
+                "uuid": uuidv4(),
+                "css": {},
+            },{
+                "uuid": uuidv4(),
+                "css": {}
+            },{
+                "uuid": uuidv4(),
+                "css": {}
+            },{
+                "uuid": uuidv4(),
+                "css": {
+                    "width": "6px",
+                    "height": "6px",
+      
+                }
+            },{
+                "uuid": uuidv4(),
+                "css": {}
+            },{
+                "uuid": uuidv4(),
+                "css": {}
+            },{
+                "uuid": uuidv4(),
+                "css": {}
+            },{
+                "uuid": uuidv4(),
+                "css": {}
+            },{
+                "uuid": uuidv4(),
+                "css": { }
+            },{
+                "uuid": uuidv4(),
+                "css": {}
+            }]
+            },
+            "page2": {
+                "defaultItem": {
+                  "width": "35px",
+                  "height": "35px",
+                  "backgroundColor": "#00B2A9"
+                },
+                "grid": [
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "height": "4px",
+                        "width": "4px"
+                    }
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "width": "30px",
+                        "height": "5px"
+                    }
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  }
+                ]
+              },
+            "page3": {
+                "defaultItem": {
+                  "width": "48px",
+                  "height": "48px",
+                  "backgroundColor": "#00B2A9"
+                },
+                "grid": [
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                    }
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "width": "9px",
+                        "height": "9px"
+                    }
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "height": "35px"
+                    }
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "width": "3px"
+                    }
+                  }
+                ]
+            },
+            "page4": {
+                "defaultItem": {
+                  "width": "20px",
+                  "height": "25px",
+                  "backgroundColor": "#00B2A9"
+                },
+                "grid": [
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "width": "10px"
+                    }
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "height": "5px"
+                    }
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "height": "50px"
+                    }
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "height": "2px",
+                        "width": "2px"
+                    }
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  }
+                ]
+            },
+            "page5": {
+                "defaultItem": {
+                  "width": "50px",
+                  "height": "50px",
+                  "backgroundColor": "#00B2A9"
+                },
+                "grid": [
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {}
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "height": "40px",
+                        "width": "40px",
+                    }
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "height": "30px",
+                        "width": "30px",
+                        
+                    }
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "height": "20px",
+                        "width": "20px",
+                    }
+                  },
+                  {
+                    "uuid": uuidv4(),
+                    "css": {
+                        "height": "5px",
+                        "width": "5px",
+                    }
+                  }
+                ]
+              },
+              "ok": ["f1e41353-a0d6-40df-99ee-4050b897588a", "7985c674-410d-42e5-89aa-948f8c96cc93", "cf511ca6-2f00-4409-8b8e-f478a7efd5fb", "460801aa-9d8f-4d16-b36f-d5913f741a58", "d6771cc3-e326-4c14-992b-ceb3a5774037"]
+        },
+};
+    return JSON.stringify(Level);
+}
+export default levelGenerator;
