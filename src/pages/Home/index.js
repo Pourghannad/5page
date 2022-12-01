b70e5d0c1230ff6de439b702d6342624d2d1edfe
Pourghannad <mhpourghannad@gmail.com>
2022-12-01 18:27:32 +0330

    add basic of level modal

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames";
 import style from "./style.module.scss";
 import { withRouter } from "react-router";
@@ -7,9 +7,12 @@ import { ReactComponent as Play } from "../../assets/play.svg";
 import { LSG } from "../../utils/store";
 const Home = (props) => {
     const [intro, setIntro] = useState(!LSG('intro'));
-    setTimeout(() => {
-        setIntro(false);
-    }, 1200);
+    const [levelModal, setLevelModal] = useState(false);
+    useEffect(() => {
+        setTimeout(() => {
+            setIntro(false);
+        }, 1200);
+    }, []);
     return (
         <div className={style["home-page-container"]}>
             <div className={classNames(style["intro-wrapper"], {[style["deactive-intro"]]: !intro})}>
@@ -18,11 +21,16 @@ const Home = (props) => {
             <React.Fragment>
                 <span className={style["fivepage-logo-type"]}>5Page</span>
                 <div className={style["action-button-container"]}>
-                    <button onClick={() => props.history.push("play?level=1")}>
+                    <button onClick={() => setLevelModal(true)}>
                         <Play />
                     </button>
                 </div>
             </React.Fragment>
+            {levelModal && 
+                <div className={style["level-modal"]}>
+                    
+                </div>
+            }
         </div>
     );
 };
