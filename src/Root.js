34c7e30f6a8bd1a2d6b5ca9e56149c1bf97dccf7
Pourghannad <mhpourghannad@gmail.com>
2023-02-07 12:51:43 +0330

    Add lazy load to router

--- src/Root.js
+++ src/Root.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { HashRouter } from 'react-router-dom'
+import React, { Suspense } from "react";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import style from "./root-style.module.scss";
 import "./reset.scss";
 import "./fonts.scss";
-import RouterSwitch from './Router';
+
+const Home = React.lazy(() => import("./pages/Home"));
+const Play = React.lazy(() => import("./pages/Play"));
+
 const Root = () => {
   return (
     <HashRouter>
       <main className={style["main-container"]}>
         <section className={style["wrapper"]}>
-          <RouterSwitch />
-          {/* <div className={style["ads-container"]}>
-            <span>ADS</span>
-          </div> */}
+          <Switch>
+            <Suspense fallback={<>Loading...</>}>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/play" component={Play} />
+            </Suspense>
+          </Switch>
         </section>
-        </main>
+      </main>
     </HashRouter>
   );
-}
+};
 
 export default Root;
