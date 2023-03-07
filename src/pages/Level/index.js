37725596f49c6c651da2cc3a4c0eca10912acc47
Pourghannad <mhpourghannad@gmail.com>
2023-03-07 15:22:40 +0330

    add level state

--- src/pages/Level/index.js
+++ src/pages/Level/index.js
@@ -1,25 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames";
 import style from "./style.module.scss";
 import { withRouter } from "react-router";
+import { LSG } from "../../utils/store";
 
-const Level = (props) => (
-    <div className={classNames(style["level-modal"], { [style["active"]]: true })}>
-        <h6>LEVEL SELECT</h6>
-        <div>
-            <span onClick={() => props.history.push("/play?level=1")}>1</span>
-            <span onClick={() => props.history.push("/play?level=2")}>2</span>
-            <span onClick={() => props.history.push("/play?level=3")}>3</span>
-            <span onClick={() => props.history.push("/play?level=4")}>4</span>
-            <span onClick={() => props.history.push("/play?level=5")}>5</span>
-            <span onClick={() => props.history.push("/play?level=6")}>6</span>
-            <span onClick={() => props.history.push("/play?level=7")}>7</span>
-            <span onClick={() => props.history.push("/play?level=8")}>8</span>
-            <span onClick={() => props.history.push("/play?level=9")}>9</span>
-            <span onClick={() => props.history.push("/play?level=10")}>10</span>
-            <span onClick={() => props.history.push("/play?level=11")}>11</span>
-        </div>
+const Level = (props) => {
+  const [level, setLevel] = useState([
+    {
+      number: 1,
+      count: 0,
+    },
+    {
+      number: 2,
+      count: 0,
+    },
+    {
+      number: 3,
+      count: 0,
+    },
+    {
+      number: 4,
+      count: 0,
+    },
+    {
+      number: 5,
+      count: 0,
+    },
+    {
+      number: 6,
+      count: 0,
+    },
+    {
+      number: 7,
+      count: 0,
+    },
+    {
+      number: 8,
+      count: 0,
+    },
+    {
+      number: 9,
+      count: 0,
+    },
+    {
+      number: 10,
+      count: 0,
+    },
+    {
+      number: 11,
+      count: 0,
+    },
+  ]);
+  useEffect(() => {
+    let levelObject = {};
+    try {
+      levelObject = JSON.parse(LSG("level"));
+    } catch (error) {
+      levelObject = {};
+    }
+    if (Object.keys(levelObject).length > 0) {
+    }
+  }, []);
+  return (
+    <div
+      className={classNames(style["level-modal"], { [style["active"]]: true })}
+    >
+      <h6>LEVEL SELECT</h6>
+      <div>
+        {level.map((item) => (
+          <span
+            key={`level-${item.number}`}
+            onClick={() => props.history.push(`/play?level=${item.number}`)}
+          >
+            {item.number}
+          </span>
+        ))}
+      </div>
     </div>
-);
+  );
+};
 
 export default withRouter(Level);
