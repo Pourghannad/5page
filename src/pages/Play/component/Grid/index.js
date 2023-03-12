f551a96b99895bd2638bf97204e50aabb1adc92e
Pourghannad <mhpourghannad@gmail.com>
2023-03-12 12:40:07 +0330

    add item objects in grid component

--- src/pages/Play/component/Grid/index.js
+++ src/pages/Play/component/Grid/index.js
@@ -1,44 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import style from "./style.module.scss";
 import classnames from "classnames";
-import { cloneObject } from '../../../../utils/lodash';
+import { cloneObject } from "../../../../utils/lodash";
 import Loading from "../../../../components/loading";
 const Grid = (props) => {
-    const [selected, setSelected] = useState([]);
-    const itemHandleClick = (param) => {
-        if (!props.standard)  {
-            if (!selected.includes(param.uuid)) {
-                setSelected([param.uuid])
-            } else {
-                setSelected(selected.filter((item) => {
-                    return item !== param.uuid
-                }));
-            }
-            props.onSelected(param.uuid);
-        }
+  const [selected, setSelected] = useState([]);
+  const itemHandleClick = (param) => {
+    if (!props.standard) {
+      if (!selected.includes(param.uuid)) {
+        setSelected([param.uuid]);
+      } else {
+        setSelected(
+          selected.filter((item) => {
+            return item !== param.uuid;
+          })
+        );
+      }
+      props.onSelected(param.uuid);
     }
-    return (
-        <div className={classnames(style["grid-container"], {[style["standard"]]: props.standard})}>
-            <div className={style["items-container"]}>
-                {props.data ? props.data.grid.map((item, key) => {
-                    const itemCss = cloneObject(item.css);
-                    const defaultCss = cloneObject(props.data.defaultItem);
-                    return (
-                        <div
-                            key={key}
-                            onClick={() => itemHandleClick(item)}
-                            className={classnames(style["item"], {
-                                [style["active"]]: item.active,
-                                [style["selected"]]: selected.includes(item.uuid),
-                            })}
-                        >
-                            <span style={Object.assign(defaultCss, itemCss)}></span>
-                        </div>
-                    );
-                }) : <Loading />}
-            </div>
-        </div>
-    );
+  };
+  return (
+    <div
+      className={classnames(style["grid-container"], {
+        [style["standard"]]: props.standard,
+      })}
+    >
+      <div className={style["items-container"]}>
+        {props.data ? (
+          props.data.grid.map((item, key) => {
+            const itemCss = cloneObject(item.css);
+            const defaultCss = cloneObject(props.data.defaultItem);
+            return (
+              <div
+                key={key}
+                onClick={() => itemHandleClick(item)}
+                className={classnames(style["item"], {
+                  [style["active"]]: item.active,
+                  [style["selected"]]: selected.includes(item.uuid),
+                })}
+              >
+                {item.objects && item.objects.length > 0 ? (
+                    <div className={style["multi-object-wrapper"]}>
+                        {item.objects.map((object) => {
+                          return (
+                            <div style={cloneObject(object)}></div>
+                          )
+                        })}
+                    </div>
+                ) : (
+                  <span style={Object.assign(defaultCss, itemCss)}></span>
+                )}
+              </div>
+            );
+          })
+        ) : (
+          <Loading />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Grid;
