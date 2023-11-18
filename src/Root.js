dff68c43bc5325d8558152c2dbebd9d5b4dc2c03
MohammadHasan Pourghannad <mhpourghannad@gmail.com>
2023-11-18 16:55:20 +0330

    add .nvmrc file

--- src/Root.js
+++ src/Root.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import style from "./root-style.module.scss";
 import Loading from "./components/loading";
@@ -12,21 +12,35 @@ const About = React.lazy(() => import("./pages/About"));
 const Editor = React.lazy(() => import("./pages/Editor"));
 
 const Root = () => {
+  const [fontReady, setFontReady] = useState(true);
+  useEffect(() => {
+    document.fonts.ready
+      .then(() => {
+        setFontReady(false);
+      })
+      .catch(() => {
+        setFontReady(false);
+      });
+  }, []);
   return (
     <HashRouter>
-      <main className={style["main-container"]}>
-        <section className={style["wrapper"]}>
-          <Switch>
-            <Suspense fallback={<Loading />}>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/level" component={Level} />
-              <Route exact path="/play" component={Play} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/editor" component={Editor} />
-            </Suspense>
-          </Switch>
-        </section>
-      </main>
+      {fontReady ? (
+        <Loading />
+      ) : (
+        <main className={style["main-container"]}>
+          <section className={style["wrapper"]}>
+            <Switch>
+              <Suspense fallback={<Loading />}>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/level" component={Level} />
+                <Route exact path="/play" component={Play} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/editor" component={Editor} />
+              </Suspense>
+            </Switch>
+          </section>
+        </main>
+      )}
     </HashRouter>
   );
 };
