e1029a82e63e86ba3acfb6a4ee9f3e43281f745c
MohammadHasan Pourghannad <mhpourghannad@gmail.com>
2023-12-19 08:38:48 +0330

    remove homeFive animation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,42 +1,52 @@
 import React, { useEffect, useState } from "react";
 import classNames from "classnames";
 import style from "./style.module.scss";
-import Intro from "./component/intro"
+import Intro from "./component/intro";
 import { ReactComponent as Play } from "../../assets/play.svg";
 import { ReactComponent as About } from "../../assets/about.svg";
 import { LSG } from "../../utils/store";
 import { withRouter } from "react-router-dom";
 const Home = (props) => {
-    const [intro, setIntro] = useState(!LSG('intro'));
-    const isLevel = LSG("level");
-    useEffect(() => {
-        setTimeout(() => {
-            setIntro(false);
-        }, 1200);
-    }, []);
-    return (
-        <div className={style["home-page-container"]}>
-            <div className={classNames(style["intro-wrapper"], {[style["deactive-intro"]]: !intro})}>
-                <Intro />
-            </div>
-            <React.Fragment>
-                <span className={style["fivepage-logo-type"]}>5Page</span>
-                <div className={style["action-button-container"]}>
-                    <button style={{display: "none"}} aria-label="About" onClick={() => props.history.push('/about')}>
-                        <About />
-                    </button>
-                    <button aria-label="Play" onClick={() => isLevel ? props.history.push('/level') : props.history.push('/play?level=1')}>
-                        <Play />
-                        <span>5</span>
-                        <span>5</span>
-                        <span>5</span>
-                        <span>5</span>
-                        <span>5</span>
-                    </button>
-                </div>
-            </React.Fragment>
+  const [intro, setIntro] = useState(!LSG("intro"));
+  const isLevel = LSG("level");
+  useEffect(() => {
+    setTimeout(() => {
+      setIntro(false);
+    }, 1200);
+  }, []);
+  return (
+    <div className={style["home-page-container"]}>
+      <div
+        className={classNames(style["intro-wrapper"], {
+          [style["deactive-intro"]]: !intro,
+        })}
+      >
+        <Intro />
+      </div>
+      <React.Fragment>
+        <span className={style["fivepage-logo-type"]}>5Page</span>
+        <div className={style["action-button-container"]}>
+          <button
+            style={{ display: "none" }}
+            aria-label="About"
+            onClick={() => props.history.push("/about")}
+          >
+            <About />
+          </button>
+          <button
+            aria-label="Play"
+            onClick={() =>
+              isLevel
+                ? props.history.push("/level")
+                : props.history.push("/play?level=1")
+            }
+          >
+            <Play />
+          </button>
         </div>
-    );
+      </React.Fragment>
+    </div>
+  );
 };
 
 export default withRouter(Home);
