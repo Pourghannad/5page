99c02283e5fcadd3dce3053d8d346a8683d59088
MohammadHasan Pourghannad <mhpourghannad@gmail.com>
2024-03-11 10:43:50 +0330

    add levelSelect.js file

--- src/pages/Level/index.js
+++ src/pages/Level/index.js
@@ -5,116 +5,11 @@ import { withRouter } from "react-router";
 import { LSG } from "../../utils/store";
 import { merge } from "../../utils/lodash";
 import { ReactComponent as StartSvg } from "../../assets/star.svg";
+import { LevelSelect } from "src/utils/levelSelect";
 
 const Level = (props) => {
   const [active, setActive] = useState(false);
-  const [level, setLevel] = useState([
-    {
-      number: 1,
-      count: {
-        correct: 0,
-        wrong: 0
-      },
-    },
-    {
-      number: 2,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 3,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 4,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 5,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 6,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 7,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 8,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 9,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 10,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 11,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 12,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 13,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 14,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-    {
-      number: 15,
-      count: {
-        correct: 0,
-        wrong: 0
-      }
-    },
-  ]);
+  const [level, setLevel] = useState(LevelSelect);
 
   useEffect(() => {
     let levelObject = [];
---
+++ src/utils/levelSelect.js
@@ -0,0 +1,107 @@
+export const LevelSelect = [
+    {
+      number: 1,
+      count: {
+        correct: 0,
+        wrong: 0
+      },
+    },
+    {
+      number: 2,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 3,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 4,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 5,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 6,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 7,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 8,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 9,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 10,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 11,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 12,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 13,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 14,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+    {
+      number: 15,
+      count: {
+        correct: 0,
+        wrong: 0
+      }
+    },
+  ];
